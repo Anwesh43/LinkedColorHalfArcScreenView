23cdcd0e136131440c161de9a0c972754068ddbd
Anwesh43 <anweshthecool0@gmail.com>
2019-09-12 01:01:10 +0530

    adding renderer in ColorHalfArcAScreenView class

--- colorhalfarcscreenview/src/main/java/com/anwesh/uiprojects/colorhalfarcscreenview/ColorHalfArcScreenView.kt
+++ colorhalfarcscreenview/src/main/java/com/anwesh/uiprojects/colorhalfarcscreenview/ColorHalfArcScreenView.kt
@@ -63,15 +63,16 @@ fun Canvas.drawCHASNode(i : Int, scale : Float, sc : Float, currI : Int, paint :
 class ColorHalfArcScreenView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
